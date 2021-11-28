94371af4a12ffc9aaf09312491c0b4abe213fa19
PashPaul <68594232+PashPaul@users.noreply.github.com>
2021-11-28 18:37:13 +0100

    Fix #9

--- spec/features/author/index_spec.rb
+++ spec/features/author/index_spec.rb
@@ -7,4 +7,12 @@ describe "Authors", type: :feature do
     expect(page).to have_text("Homepage")
     expect(page).to have_link 'New author', href: new_author_path
   end
+  it "should have a link to delete an author" do
+    @author = FactoryBot.create :author
+    visit authors_path
+    expect(page).to have_link 'Delete'
+    expect(Author.count).to eq(1)
+    find('a[data-method="delete"]').click
+    expect(Author.count).to eq(0)
+  end
 end
