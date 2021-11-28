698aba24352e31765fd436a1a48d5554ff230563
PashPaul <68594232+PashPaul@users.noreply.github.com>
2021-11-28 21:48:15 +0100

    Fix #11

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe Paper, type: :model do
     expect(paper.venue).to eq("Ort")
     expect(paper.year).to eq(2021)
   end
+  it "should not validate without title, venue, year" do
+    paper = Paper.new(title: "1", venue: "", year: 2)
+    expect(paper).to_not be_valid
+    paper = Paper.new(title: "", venue: "1", year: 2)
+    expect(paper).to_not be_valid
+    paper = Paper.new(title: "1", venue: "2")
+    expect(paper).to_not be_valid
+  end
 end
